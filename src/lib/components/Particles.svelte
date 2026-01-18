1868d0e598eb2bbcbfde1ca2a443dcf06a0f7736
LOLYAY <lolyay@lolyay.de>
2026-01-18 22:42:33 +0100

    Add `@iconify/svelte` dependency, integrate particles.js functionality, update layout and homepage styles, allow all crawling with `robots.txt`, and include Fabric API mod card.
    (cherry picked from commit 316d978a178af4c0feb8bbc5a56705711d7e550f)

---
+++ src/lib/components/Particles.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const Particles = (await import('$lib/particles-js.js')).default;
+		Particles.init({
+			selector: '.particles-js-canvas-el'
+		});
+	});
+</script>
+
+
+<div id="particles-js">
+	<canvas class="particles-js-canvas-el"></canvas>
+</div>
+
+<style>
+    .particles-js-canvas-el {
+        z-index: -1000;
+        position: absolute;
+        display: block;
+        top: 0;
+        left: 0;
+    }
+
+</style>
